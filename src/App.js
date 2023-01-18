a8cd8c9a43746fac0d44d4956c2552f2120c1097
DevGibby <71801512+DevGibby@users.noreply.github.com>
2023-01-17 19:48:30 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import './App.css';
+import { Data } from './Data.js';
+
+export default function App(){
+	return(
+    <div className="app">
+      {
+        Data.map((bill, key) => {
+          return(
+            <MyBill
+              bill={bill}
+              key={key}
+            />
+          )
+        })
+      }
+    </div>
+  )
+}
+
+const MyBill = ({ bill }) => {
+
+  const handleStatusStyling = (status) => {
+    if(status === 'Paid'){
+      return { background: 'lightgreen'}
+    } 
+  }
+
+	return(
+    <div className="my-bill-container" style={handleStatusStyling(bill.status)}>
+      <h2>{bill.type}</h2>
+      <h2>Amount: {bill.amount}</h2>
+      <h2>Status: {bill.status}</h2>
+    </div>
+  )
+}
---
+++ src/Data.js
@@ -0,0 +1,47 @@
+export const Data = [
+    {
+        type: 'Water',
+        amount: 75,
+        status: 'Due',
+    },
+    {
+        type: 'Gas',
+        amount: 110,
+        status: 'Paid',
+    },
+    {
+        type: 'Insurance',
+        amount: 80,
+        status: 'Overdue',
+    },
+    {
+        type: 'Taxes',
+        amount: 65,
+        status: 'Due',
+    },
+    {
+        type: 'Water',
+        amount: 120,
+        status: 'Due',
+    },
+    {
+        type: 'Water',
+        amount: 50,
+        status: 'Overdue',
+    },
+    {
+        type: 'YouTube',
+        amount: 60,
+        status: 'Due',
+    },
+    {
+        type: 'Netflix',
+        amount: 70,
+        status: 'Due',
+    },
+    {
+        type: 'Electric',
+        amount: 90,
+        status: 'Paid',
+    },
+]
